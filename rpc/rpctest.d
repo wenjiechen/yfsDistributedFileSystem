d85ec84864702b70699c994f5667180a7d2d3773
Wenjie Chen <wenjie.chen@nyu.edu>
2013-10-11 14:24:54 -0400

    solution for lab 1

--- rpc/rpctest.d
+++
@@ -1,3 +0,0 @@
-rpc/rpctest: rpc/rpctest.cc rpc/rpc.h rpc/thr_pool.h rpc/fifo.h \
- rpc/slock.h lang/verify.h rpc/marshall.h lang/algorithm.h \
- rpc/connection.h rpc/pollmgr.h rpc/jsl_log.h gettime.h
